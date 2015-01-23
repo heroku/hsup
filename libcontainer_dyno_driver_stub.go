1d909e2f60c068f61dc3170519b1d15b4e50c04a
Fabio Kung <fabio.kung@gmail.com>
2015-01-22 20:23:50 -0800

    no support for libcontainer drivers  on darwin
    binaries can be built on OSX, without support for the libcontainer driver

---
+++ libcontainer_dyno_driver_stub.go
@@ -0,0 +1,50 @@
+// +build !linux
+
+package hsup
+
+import "errors"
+
+var (
+	ErrDriverNotSupported = errors.New(
+		"the libcontainer driver is not supported on this platform",
+	)
+)
+
+type LibContainerDynoDriver struct{}
+
+func NewLibContainerDynoDriver(string) (*LibContainerDynoDriver, error) {
+	return nil, ErrDriverNotSupported
+}
+func (dd *LibContainerDynoDriver) Build(*Release) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerDynoDriver) Start(*Executor) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerDynoDriver) Stop(*Executor) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerDynoDriver) Wait(*Executor) *ExitStatus {
+	return nil
+}
+
+type LibContainerInitDriver struct{}
+
+func (dd *LibContainerInitDriver) Build(*Release) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerInitDriver) Start(*Executor) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerInitDriver) Stop(*Executor) error {
+	return ErrDriverNotSupported
+}
+
+func (dd *LibContainerInitDriver) Wait(*Executor) *ExitStatus {
+	return nil
+}
