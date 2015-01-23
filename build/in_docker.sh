28f09fe06be5a4f65a1ebe62dee71a576cc9c528
Scott Clasen <scott@heroku.com>
2015-01-23 09:35:03 -0800

    build godep in linux and use it to build linux side of hsup

--- build/in_docker.sh
+++ build/in_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd /go/src/github.com/heroku/hsup
-godep go build -v -o hsup-linux-amd64 ./cmd/hsup
+go build -o godep-linux-amd64 github.com/tools/godep
+./godep-linux-amd64 go build -v -o hsup-linux-amd64 ./cmd/hsup
