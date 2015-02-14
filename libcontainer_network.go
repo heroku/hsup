95aaa22ea2e50c496f6829c74661702843a200b7
Fabio Kung <fabio.kung@gmail.com>
2015-02-13 17:46:56 -0800

    libcontainer: create the veth pair and set IPs
    Implemented as a custom network strategy for libcontainer. It is mostly
    the same as the default network.Veth strategy, with the exception that
    it doesn't try to add the master veth interface to a bridge, and
    configures it to be the gateway.

--- libcontainer_network.go
+++ libcontainer_network.go
@@ -6,14 +6,104 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"net"
+
+	"github.com/docker/libcontainer/netlink"
+	"github.com/docker/libcontainer/network"
+	"github.com/docker/libcontainer/utils"
 )
 
-var ErrInvalidIPMask = errors.New("mask is not a /30")
+var (
+	ErrInvalidIPMask = errors.New("mask is not a /30")
+)
+
+func init() {
+	network.AddStrategy("routed", &Routed{})
+}
+
+// Routed implements libcontainer's network.NetworkStrategy interface,
+// offering containers only layer 3 connectivity to the outside world.
+type Routed struct {
+	network.Veth
+}
+
+// Create sets up a veth pair, setting the config.Gateway address on the master
+// (host) side. The veth pair forms a small subnet with a single host and
+// gateway.
+func (r *Routed) Create(
+	config *network.Network, nspid int, state *network.NetworkState,
+) error {
+	// TODO: ensure that config.Gateway and config.Address are in the same subnet
+	if config.VethPrefix == "" {
+		return fmt.Errorf("veth prefix is not specified")
+	}
+	name1, name2, err := createVethPair(config.VethPrefix, config.TxQueueLen)
+	if err != nil {
+		return err
+	}
+	_, subnet, err := net.ParseCIDR(config.Address)
+	if err != nil {
+		return err
+	}
+	gw := &net.IPNet{
+		IP:   net.ParseIP(config.Gateway),
+		Mask: subnet.Mask,
+	}
+	if err := network.SetInterfaceIp(name1, gw.String()); err != nil {
+		return err
+	}
+	if err := network.SetMtu(name1, config.Mtu); err != nil {
+		return err
+	}
+	if err := network.InterfaceUp(name1); err != nil {
+		return err
+	}
+	if err := network.SetInterfaceInNamespacePid(name2, nspid); err != nil {
+		return err
+	}
+	state.VethHost = name1
+	state.VethChild = name2
+	return nil
+}
+
+func (r *Routed) Initialize(
+	net *network.Network, state *network.NetworkState,
+) error {
+	return r.Veth.Initialize(net, state)
+}
+
+// createVethPair will automatically generage two random names for
+// the veth pair and ensure that they have been created
+//
+// Copied from libcontainer/network.createVethPair because it is not exported
+func createVethPair(prefix string, txQueueLen int) (name1 string, name2 string, err error) {
+	for i := 0; i < 10; i++ {
+		if name1, err = utils.GenerateRandomName(prefix, 7); err != nil {
+			return
+		}
+
+		if name2, err = utils.GenerateRandomName(prefix, 7); err != nil {
+			return
+		}
+
+		if err = network.CreateVethPair(name1, name2, txQueueLen); err != nil {
+			if err == netlink.ErrInterfaceExists {
+				continue
+			}
+
+			return
+		}
+
+		break
+	}
+
+	return
+}
 
 // smallSubnet encapsulates operations on single host /30 IPv4 networks. They
 // contain only 4 ip addresses, and only one of them is usable for hosts:
-// network address, gateway ip, host ip and broadcast ip.
+// 1) network address, 2) gateway ip, 3) host ip, and 4) broadcast ip.
 type smallSubnet struct {
 	subnet    *net.IPNet
 	gateway   *net.IPNet
